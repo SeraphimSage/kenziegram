cbfc41ce2f5d785ff5f713c46597201c8b2e64d2
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-04-18 12:39:54 -0600

    complete pug seperation and add css styling back to page.

--- app.js
+++ app.js
@@ -20,70 +20,15 @@ const upload = multer({ storage: storage });
 const uploaded_files = [];
 
 app.get("/", (req, res) => {
-	res.render("index", { title: "Kenziegram" });
 	const path = "./public/uploads";
-	// fs.readdir(path, (err, items) => {
-	// 	const images = items.map((image) => {
-	// 		return `<img style = 'height:200px' src='./uploads/${image}' />`;
-	// 	});
-	// 	res.send(`
-	// 	<style>
-	// 		body {
-	// 		background-color: darkgray;
-	// 	}
-
-	// 	h1 {
-	// 		text-align: center;
-	// 	}
-	// 	#pictureForm {
-	// 		position: relative;
-	// 		align-self: center;
-	// 		text-align: center;
-	// 	}
-
-	// 	label, #image, button {
-	// 		padding: 10px;
-	// 	}
-
-	// 	h2 {
-	// 		text-align: center;
-	// 	}
-
-	// 	#sentPictures {
-	// 		display: flex;
-	// 		justify-content: center;
-	// 		align-content: center;
-	// 		flex-direction: row-reverse;
-	// 	}
-
-	// 	img {
-	// 		flex-basis: auto;
-	// 		border-radius: 50%;
-	// 	}
-
-	// 	</style>
-	// 	<h1>Kenziegram</h1>
-	//         <form id="pictureForm" method='post' action='/upload' enctype='multipart/form-data'>
-	//             <div >
-	// 				<label for='image'>Choose a picture</label>
-	// 				<br />
-	//                 <input type="file" id="image" name="image">
-	// 			</div>
-	//             <div>
-	//                 <button>Send the picture</button>
-	//             </div>
-	//         </form>
-	// 		<h2>Sent Pictures</h2>
-	// 		<div id="sentPictures">
-	//         ${images}
-	// 		</div>
-	//     `);
-	// });
+	fs.readdir(path, (err, items) => {
+		const images = items.map((image) => {
+			res.render("index", { title: "Kenziegram", images: items });
+		});
+	});
 });
 
 app.post("/upload", upload.single("image"), function (request, response, next) {
-	// request.file is the \`myFile\` file
-	// request.body will hold the text fields, if there were any
 	uploaded_files.push(request.file.filename);
 	console.log("Uploaded: " + request.file.filename);
 	response.redirect("/");
